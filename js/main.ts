682fffc2c32157f1237baaba21a8967faadc9cf2
Dylan Simon <dylan@dylex.net>
2018-03-05 18:01:49 -0500

    Add filter remove buttons

--- js/main.ts
+++ js/main.ts
@@ -17,18 +17,18 @@ type Properties = Dict<{
 
 type Catalog = {
   name: string,
-  query: {method: string, uri: string},
+  query: {method: string, uri: string, csv: string},
   fixed: string[],
   count?: number,
   props: Properties
 };
 
-type Query = undefined|string|number|{lb:number, ub:number};
+type Query = undefined|string|number|{lb:string|number, ub:string|number};
 
 interface Filter {
   name: string;
   query: Query;
-  update_aggs: (aggs: Dict<any>) => void;
+  update_aggs?: (aggs: Dict<any>) => void;
 }
 
 var TCat: DataTables.Api|null = null;
@@ -36,44 +36,44 @@ const Catalog: Catalog = (<any>window).Catalog;
 const Filters: Array<Filter> = [];
 var Update_aggs: number = 0;
 
-function select_options(select: HTMLSelectElement, options: string[]) {
+function select_options(select: HTMLSelectElement, options: string[], info: string[] = []) {
   $(select).empty();
   select.appendChild(document.createElement('option'));
-  for (let v of options) {
+  for (let i = 0; i < options.length; i++) {
+    let v = options[i];options[i];options[i];options[i];
     let opt = document.createElement('option');
     opt.setAttribute('value', v);
     opt.textContent = v;
+    if (info[i])
+      opt.textContent += ' (' + info[i] + ')';
     select.appendChild(opt);
   }
 }
 
 function set_download(query: Dict<string>) {
-  let a = document.getElementById('download.csv');
+  let a = <HTMLAnchorElement>document.getElementById('download.csv');
   if (!a)
-    a = $('#download').html('download as <a id="download.csv">csv</a>').children('a')[0];
+    a = <HTMLAnchorElement>$('#download').html('download as <a id="download.csv">csv</a>').children('a')[0];
   a.href = Catalog.query.csv + '?' + $.param(query);
 }
 
 function ajax(data: any, callback: ((data: any) => void), opts: any) {
-  const aggs: string[] = [];
   const query: any = {
     sort: data.order.map((o: any) => {
       return (o.dir == "asc" ? '' : '-') + data.columns[o.column].data;
     }).join(',')
   };
-  for (let fi = 0; fi < Filters.length; fi++) {
+  for (let fi = 0; fi < Update_aggs; fi++) {
     const filt = Filters[fi];
-    if (fi < Update_aggs) {
-      if (filt.query != null)
-        query[filt.name] = typeof filt.query == 'object' ? filt.query.lb+','+filt.query.ub : filt.query;
-    } else {
-      aggs.push(filt.name);
-    }
+    if (filt.query != null)
+      query[filt.name] = typeof filt.query == 'object' ? filt.query.lb+','+filt.query.ub : filt.query;
   }
+  const aggs = Filters.slice(Update_aggs);
   set_download(query);
   query.offset = data.start;
   query.limit = data.length;
-  query.aggs = aggs.join(',');
+  if (aggs)
+    query.aggs = aggs.map((filt) => filt.name).join(',');
   $.ajax({
     method: Catalog.query.method,
     url: Catalog.query.uri,
@@ -86,8 +86,10 @@ function ajax(data: any, callback: ((data: any) => void), opts: any) {
       recordsFiltered: res.hits.total,
       data: res.hits.hits
     });
-    for (let fi = Update_aggs; fi < Filters.length; fi++) {
-      Filters[fi].update_aggs(res.aggregations[Filters[fi].name]);
+    for (let filt of aggs) {
+      let f = filt.update_aggs;
+      if (f)
+        f(res.aggregations[filt.name]);
     }
     Update_aggs = Filters.length;
   }, (xhr, msg, err) => {
@@ -99,12 +101,13 @@ function ajax(data: any, callback: ((data: any) => void), opts: any) {
   });
 }
 
-function add_filt_row(name: string, ...nodes: Array<JQuery.htmlString | JQuery.TypeOrArray<JQuery.Node | JQuery<JQuery.Node>>>): HTMLTableRowElement {
-  let id = 'filt.'+name;
+function add_filt_row(name: string, ...nodes: Array<JQuery.htmlString | JQuery.TypeOrArray<JQuery.Node | JQuery<JQuery.Node>>>) {
+  let id = 'filt-'+name;
   let tr = <HTMLTableRowElement|null>document.getElementById(id);
-  if (tr) return tr;
+  if (tr) return;
   let tab = <HTMLTableElement>document.getElementById('filt');
   tr = document.createElement('tr');
+  tr.id = id;
   if (tab.lastChild)
     $(tr).insertBefore(<HTMLTableRowElement>tab.lastChild);
   else
@@ -113,40 +116,53 @@ function add_filt_row(name: string, ...nodes: Array<JQuery.htmlString | JQuery.T
     let td = $(document.createElement('td')).appendTo(tr);
     td.append(node);
   }
-  return tr;
 }
 
 function add_filter(name: string) {
-  if (Filters.some((f) => f.name == name))
+  if (!TCat || Filters.some((f) => f.name == name))
     return;
   let isint: boolean = false;
+  const filt: Filter = {
+    name: name,
+    query: undefined,
+  };
+  const update = () => {
+    let i = Filters.indexOf(filt);
+    if (i >= 0 && Update_aggs > i)
+      Update_aggs = i+1;
+  };
+  let label = $(document.createElement('span'));
+  label.append(
+    $('<button class="remove">&times;</button>').on('click', function () {
+      if (!TCat) return;
+      let i = Filters.indexOf(filt);
+      if (i < 0) return;
+      Filters.splice(i, 1);
+      $('tr#filt-'+name).remove();
+      Update_aggs = i;
+      TCat.column(name).search('').draw();
+    }),
+    name);
   switch (Catalog.props[name].type) {
     case 'keyword': {
       let select = document.createElement('select');
       select.name = name;
-      let filt: Filter = {
-        name: name,
-        query: undefined,
-        update_aggs: (aggs: any) => {
-          select_options(select, aggs.buckets.map((b: any) => b.key));
-          select.value = '';
-        }
+      filt.update_aggs = (aggs: any) => {
+        select_options(select, aggs.buckets.map((b: any) => b.key), aggs.buckets.map((b: any) => b.doc_count));
+        select.value = '';
       };
-      let next = Filters.push(filt);
       let onchange = function () {
-        if (!TCat)
-          return;
+        if (!TCat) return;
         let val = select.value;
         if (val)
           filt.query = val;
         else
           filt.query = undefined;
-        Update_aggs = Math.min(Update_aggs, next);
+        update();
         TCat.column(name).visible(!val).search(val).draw();
       };
-      onchange();
       select.onchange = onchange;
-      add_filt_row(name, name, select);
+      add_filt_row(name, label, select);
       break;
     }
     case 'byte':
@@ -163,19 +179,13 @@ function add_filter(name: string) {
       lb.type = ub.type = "number";
       lb.step = ub.step = isint ? <any>1 : "any";
       let avg = document.createElement('span');
-      let filt: Filter = {
-        name: name,
-        query: undefined,
-        update_aggs: (aggs: any) => {
-          lb.defaultValue = lb.value = lb.min = ub.min = aggs.min;
-          ub.defaultValue = ub.value = lb.max = ub.max = aggs.max;
-          avg.textContent = aggs.avg;
-        }
+      filt.update_aggs = (aggs: any) => {
+        lb.defaultValue = lb.value = lb.min = ub.min = aggs.min;
+        ub.defaultValue = ub.value = lb.max = ub.max = aggs.max;
+        avg.textContent = aggs.avg;
       };
-      let next = Filters.push(filt);
       let onchange = function () {
-        if (!TCat)
-          return;
+        if (!TCat) return;
         let lbv = lb.valueAsNumber;
         let ubv = ub.valueAsNumber;
         if (lbv == ubv)
@@ -185,17 +195,20 @@ function add_filter(name: string) {
             lb:isFinite(lbv) ? lbv : '',
             ub:isFinite(ubv) ? ubv : ''
           };
-        Update_aggs = Math.min(Update_aggs, next);
+        update();
         TCat.column(name).search(lbv+" TO "+ubv).draw();
       };
-      onchange();
       lb.onchange = ub.onchange = onchange;
-      add_filt_row(name, name,
+      add_filt_row(name, label,
         $('<span>').append(lb).append(' &ndash; ').append(ub),
         $('<span><em>M</em> = </span>').append(avg));
       break;
     }
+    default:
+      return;
   }
+  Filters.push(filt);
+  TCat.column(name).search('').draw();
 }
 
 function init() {
@@ -214,11 +227,7 @@ function init() {
   addfilt.onchange = function () {
     add_filter(addfilt.value);
   };
-  if (Catalog.fixed)
-    Catalog.fixed.forEach(add_filter);
   Update_aggs = 0;
-  if (TCat)
-    TCat.destroy();
   TCat = $('table#tcat').DataTable({
     serverSide: true,
     ajax: ajax,
@@ -227,6 +236,8 @@ function init() {
     pageLength: 50,
     dom: 'l<"#download">rtip',
   });
+  if (Catalog.fixed)
+    Catalog.fixed.forEach(add_filter);
 }
 
 $(() => {
