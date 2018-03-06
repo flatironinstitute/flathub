1c9f243066263ebaa42355dc7908329bd467a430
Dylan Simon <dylan@dylex.net>
2018-03-05 21:49:50 -0500

    Add schema and import for gaea

---
+++ es/gaea/import.py
@@ -0,0 +1,62 @@
+#!/bin/env python
+
+import argparse
+import elasticsearch
+import elasticsearch.helpers
+import h5py
+import numpy
+import os
+
+argp = argparse.ArgumentParser(description="gaea catalog elastic search import")
+argp.add_argument('-I', '--index', default='gaea', help="Index name")
+argp.add_argument('-T', '--type', default='catalog', help="Type name")
+argp.add_argument('file', nargs='+', help="Simulation files")
+args = argp.parse_args()
+
+es = elasticsearch.Elasticsearch()
+
+mapping = es.indices.get_mapping(index=args.index, doc_type=args.type)[args.index]['mappings'][args.type]['properties']
+
+suffixes = {
+    2: ['x','y'],
+    3: ['x','y','z'],
+}
+
+types = {
+    'float64': 'double',
+    'float32': 'float',
+    'uint64': 'long',
+    'uint32': 'integer',
+}
+
+def generate():
+    basedoc = {'_index':args.index,'_type':args.type}
+    for name in args.file:
+        base, _ = os.path.splitext(os.path.basename(name))
+        baseid = "%s_"%(base)
+        print(base)
+        with h5py.File(name, 'r') as f:
+            keys = []
+            for k, v in f.iteritems():
+                if len(v.shape) > 1:
+                    for j, s in enumerate(suffixes[v.shape[1]]):
+                        keys.append((k+'_'+s, v[:,j]))
+                else:
+                    keys.append((k, v))
+            doc = basedoc
+            for n, v in keys:
+                assert(mapping[n]['type'] == types[v.dtype.name])
+                doc[n] = None
+            for i in xrange(0, keys[0][1].shape[0]):
+                doc['_id'] = baseid + str(i)
+                for n, v in keys:
+                    doc[n] = v[i].item()
+                yield doc
+
+n = 0
+for r in elasticsearch.helpers.streaming_bulk(es, generate()):
+    n += 1
+    if n % 50000 == 0:
+        print(n)
+print(n)
+es.indices.refresh(index=args.index)
