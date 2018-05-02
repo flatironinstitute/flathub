3a096193bd31346f22b87f7dd1f2d9494a2d111d
Dylan Simon <dylan@dylex.net>
2018-05-02 17:30:16 -0400

    Allow ingesting directories

--- Ingest.hs
+++ Ingest.hs
@@ -5,7 +5,11 @@ module Ingest
   ) where
 
 import           Control.Arrow (first)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.List (sort)
+import           Data.Maybe (isJust, fromMaybe)
 import           Data.Word (Word64)
+import           System.Directory (doesDirectoryExist, listDirectory)
 import           System.FilePath (takeExtension, splitExtension)
 import           Text.Read (readMaybe)
 
@@ -15,13 +19,22 @@ import Ingest.CSV
 import Ingest.HDF5
 
 ingest :: Catalog -> String -> M Word64
-ingest cat fno =
-  (case takeExtension $ dropz fn of
-    ".hdf5" -> ingestHDF5
-    ".csv" -> ingestCSV
-    _ -> error $ "Unknown ingest file type: " ++ fn)
-    cat blockSize fn off
+ingest cat fno = do
+  d <- liftIO $ doesDirectoryExist fn
+  if d
+    then do
+      l <- liftIO $ drop (fromIntegral off) . sort . filter (isJust . proc) <$> listDirectory fn
+      sum <$> mapM (\f -> do
+        liftIO $ putStrLn f
+        ing f 0) l
+    else ing fn off
   where
+  ing f = fromMaybe (error $ "Unknown ingest file type: " ++ f) (proc f)
+    cat blockSize f
+  proc f = case takeExtension $ dropz f of
+    ".hdf5" -> Just ingestHDF5
+    ".csv" -> Just ingestCSV
+    _ -> Nothing
   dropz f = case splitExtension fn of
     (b, ".gz") -> b
     (b, ".bz2") -> b
