05187b647d27ccfe84c84a81706955969937c1db
lizlove <elizabethlovero@gmail.com>
2020-04-10 14:57:21 -0400

    Remove mutation observer.

--- web/catalog.ts
+++ web/catalog.ts
@@ -751,32 +751,3 @@ export function initCatalog(table: JQuery<HTMLTableElement>) {
   toggleShowData(false);
   update();
 }
-
-// highcharts-data-table
-export function onElementInserted(
-  containerSelector: any,
-  elementSelector: any,
-  callback: any
-) {
-  var onMutationsObserved = function (mutations) {
-    mutations.forEach(function (mutation) {
-      if (mutation.addedNodes.length) {
-        var elements = $(mutation.addedNodes).find(elementSelector);
-        for (var i = 0, len = elements.length; i < len; i++) {
-          callback(elements[i]);
-        }
-      }
-    });
-  };
-
-  var target = $(containerSelector)[0];
-  var config = { childList: true, subtree: true };
-  var MutationObserver =
-    window.MutationObserver || window.WebKitMutationObserver;
-  var observer = new MutationObserver(onMutationsObserved);
-  observer.observe(target, config);
-}
-
-onElementInserted("body", ".highcharts-data-table", function (element) {
-  console.log(element);
-});
