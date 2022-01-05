d19f554d9ed24b74959f4639cb64d2c3c8644632
lizlove <elizabethlovero@gmail.com>
2022-01-05 12:41:39 -0500

    Fire dropdown and landing.

--- src/Html.hs
+++ src/Html.hs
@@ -116,6 +116,7 @@ htmlResponse _req hdrs body = do
             <li .header__link--dropdown>
               <a href="@{topPage !:? mempty}">Catalogs
               <div .dropdown-content .dropdown-second>
+                <a href="/fire"><text>FIRE
                 $forall (key, cat) <- catalogsSorted cats
                   <a href="@{catalogPage !:? key}">
                     <text>#{catalogTitle cat}
@@ -188,6 +189,8 @@ topPage = getPath R.unit $ \() req -> do
                     <div .box-copy>
                       <div .box-head>Catalogs
                     <ul .link-list>
+                        <li>
+                          <a .underline href="/fire">FIRE
                       $forall (sim, cat) <- catalogsSorted cats
                         <li>
                           <a .underline href="@{catalogPage !:? sim}">#{catalogTitle cat}
