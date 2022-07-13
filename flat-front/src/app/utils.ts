49f69ed0b97d443a88810615f25c3966c0ff6643
Elizabeth Lovero <elizabethlovero@gmail.com>
2022-07-13 16:00:59 -0400

    feat: add static landing for demo.

--- flat-front/src/app/utils.ts
+++
@@ -1,3 +0,0 @@
-export const classNames = function (...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-};
--- flat-front/src/types/Catalog.ts
+++
@@ -1,6 +0,0 @@
-export interface ResponseCatalog {
-    "name": "string",
-    "title": "string",
-    "synopsis": "string",
-    "order": "string"
-}
