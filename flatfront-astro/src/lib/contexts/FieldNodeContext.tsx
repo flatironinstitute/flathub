cedd828008464419bbcf7731777f979fd7877206
Paul Murray <plmrry@gmail.com>
2024-01-23 13:49:56 -0500

    remove flatfront-astro and add to gitignore

--- flatfront-astro/src/lib/contexts/FieldNodeContext.tsx
+++
@@ -1,7 +0,0 @@
-import type { CatalogHierarchyNode } from "../types";
-import { create_context_helper } from "../shared";
-
-const [useFieldNode, Provider] =
-  create_context_helper<CatalogHierarchyNode>(`FieldNode`);
-
-export { useFieldNode, Provider as FieldNodeProvider };
