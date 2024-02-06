809187d917cea3cb7dc3ff1c99c944510c1298c6
Paul Murray <plmrry@gmail.com>
2024-02-06 16:06:28 -0500

    add badges with enumerable options to FieldInfoDialog

--- flatfront/src/components/FieldInfoDialog.tsx
+++ flatfront/src/components/FieldInfoDialog.tsx
@@ -14,6 +14,8 @@ import {
   DialogTrigger
 } from "@/components/ui/dialog";
 import { Label } from "./ui/label";
+import { get_field_type, join_enums } from "@/utils";
+import { Badge } from "./ui/badge";
 
 function DialogValue({ children }: { children: React.ReactNode }) {
   return <div className="text-primary/50">{children}</div>;
@@ -61,12 +63,31 @@ export function FieldInfoDialog({
       </>
     );
   })();
+  const enumerable_options = (() => {
+    const field_type = get_field_type(metadata);
+    const is_enumerable =
+      field_type === "ENUMERABLE_INTEGER" ||
+      field_type === "LABELLED_ENUMERABLE_INTEGER" ||
+      field_type === "LABELLED_ENUMERABLE_BOOLEAN";
+    if (!is_enumerable) return null;
+    const joined = join_enums(metadata);
+    const badges = joined.map(({ text, count }, index) => {
+      const count_string = count ? ` (${d3Format(`,`)(count)} rows)` : ``;
+      return (
+        <Badge key={index} variant="outline">
+          {text} {count_string}
+        </Badge>
+      );
+    });
+    return <div className="flex flex-wrap gap-x-2 gap-y-2">{badges}</div>;
+  })();
   const fields = [
     ["Variable Name", metadata.name],
     metadata.descr ? ["Description", <Katex>{metadata.descr}</Katex>] : null,
     metadata.units ? ["Units", <Katex>{metadata.units}</Katex>] : null,
     ["Type", metadata.type],
-    children_list
+    children_list,
+    enumerable_options ? ["Options", enumerable_options] : null
   ]
     .filter(Boolean)
     .filter((d) => d[1]) as [string, React.ReactNode][];
